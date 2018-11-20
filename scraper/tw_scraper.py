e3a2e5092e4c99ad1fbdb62542ef0b23c8665ed6
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2018-11-20 21:00:18 +0900

    Remove old data file

--- scraper/tw_scraper.py
+++ scraper/tw_scraper.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 import twint
+import os
+# 이전 파일 삭제 
+file = './data/tweets.csv'
+if os.path.isfile(file):
+    os.remove(file)
 
 c = twint.Config()
 c.Search = "문재인"
