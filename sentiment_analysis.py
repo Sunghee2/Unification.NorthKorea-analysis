4342f4bb8f179bead087d4b2b5ef3713213a96d6
Sunghee2 <maria_dev@sandbox-hdp.hortonworks.com>
2018-11-20 22:41:12 +0000

    Add sentiment_analysis.py

---
+++ sentiment_analysis.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+# from pyspark.ml.evaluation import RegressionEvaluator
+# from pyspark.ml.recommendation import ALS
+from pyspark.sql import Row
+from pyspark.sql.functions import explode
+
+spark = SparkSession.builder.appName("sentiment_analysis").getOrCreate()
+
+# tweets = spark.read.load("hdfs:///user/maria_dev/data/tweet_test.csv",
+tweets = spark.read.load("./data/tweet_test.csv",
+                    format="csv", sep=",", inferSchema="true", header="true")
+training_data = spark.read.load("./data/sentiment_training_data/subjectivity-polarity.csv",
+                    format="csv", sep=",", inferSchema="true", header="true")
+                
+tweets.createOrReplaceTempView("tweets")
+training_data.createOrReplaceTempView("training_data")
+
+result = spark.sql("""
+    SELECT username 
+    FROM tweets
+    LIMIT 10
+""")
+
+for row in result.collect():
+    print(row.username)
