2fbd9b21d69b99846f2eeee0836b249a15b72d89
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2018-11-26 00:14:27 +0900

    Update mk_training_data.py
    같은 형태소 들어가있는 행 중복 문제 해결

--- mk_training_data.py
+++ mk_training_data.py
@@ -29,6 +29,7 @@ def get_tags(text):
     # str_pos = ";".join(map(str, pos))
     str_pos = ";".join(map("/".join, pos))
     return str_pos
+    # return pos
 
 
 df = read_data('data/tweet_test.csv')
@@ -36,28 +37,56 @@ df = read_data('data/tweet_test.csv')
 df['tweet'] = df.tweet.astype(str)
 
 komoran = Komoran()
-for row in df.itertuples():
+for row in df.head().itertuples():
+    print(row.index)
     word_str = get_tags(row.tweet)
     df.at[row.Index, 'word'] = word_str
-    df.set_value(row.Index, 'word', word_str)
+    # df.set_value(row.Index, 'word', word_str)
 
+print("word done....")
 # ngram list 나누기
 df1['ngram'] = df1['ngram'].str.split(';').tolist()
+df['word'] = df['word'].str.split(';').tolist()
 
 # max.prop 더한 값 넣을 새로운 column 만들기
-df['prop'] = 0
+df['prop'] = 0.0
 
-# 나중에 head없애기
-for index1, row1 in df.iterrows():
+def contains(small, big):
+    for i in list(range(len(big)-len(small)+1)):
+        for j in list(range(len(small))):
+            if big[i+j] != small[j]:
+                break
+        else:
+            return True
+    return False
+
+# # 나중에 head없애기
+for index1, row1 in df.head().iterrows():
+    sum = 0
+    incld = True
+    prop = 0
     for index, row in df1.iterrows():
-        for n in row.ngram:
-            if(n in row1.word):
+        if(incld == True or len(row.ngram) == 1) :
+            if(contains(row.ngram, row1.word)):       
+                incld = True     
                 if(row['max.value'] == 'POS'): # 긍정일 경우 더함
-                    row1.prop = row1.prop + row['max.prop']
+                    print("긍정" + str(row['max.prop']))
+                    prop = row['max.prop']
                 else: # 부정일 경우 뺌
-                    row1.prop = row1.prop - row['max.prop']
+                    print("부정!!" + str(-row['max.prop']))
+                    prop = -row['max.prop']
+            else:
+                incld = False
+                if(prop != 0):
+                    sum += prop
+                    prop = 0
+    if(sum != 0):
+        df.at[index1, 'prop'] = sum
 
 # test
 # print(df1.head(30))
-print(df.head(30))
-# print(len(df1.index))
+# print(df.head(30))
+print("ㅇㅕ기부터")
+print(df['prop'].head())
+print(df['word'].head())
+print(len(df.index))
