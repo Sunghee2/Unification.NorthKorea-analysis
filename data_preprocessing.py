9bf3fd35ed1f5859ff8d3425fd59fa3c888de23d
Sunghee Lee <31603479+Sunghee2@users.noreply.github.com>
2018-12-01 01:33:01 +0900

    정리

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -14,64 +14,21 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 def read_data(filepath):
-    # usecols = [
-    #     'id', 'conversation_id', 'date', 'time',
-    #     'username', 'name', 'tweet'
-    # ]
-    # usecols = [
-    #     'date', 'time', 'tweet'
-    # ]
-    # dtype = {
-    #     'id':
-    # }
-
     df = pd.read_csv(filepath,
-        # usecols=usecols,
         parse_dates=[['date', 'time']],
         dtype={
             'username': str
         },
         error_bad_lines=False,
         warn_bad_lines=True
-        # encoding='utf-8'
     )
     return df
-
-# def preprocess_text(text):
-
-
-
-# 형태소 분리
-# def get_nouns(text):
-#     nouns = h.nouns(text)
-
-#     # count = Counter(nouns)
-#     # print(nouns)
-#     str_nouns = "|".join(nouns)
-#     return str_nouns
 
 def get_nouns(text):
     pos = h.pos(text, ntags=22, flatten=True)
     nouns = [item for item in pos if item[1] == 'NC' or item[1] == 'NQ' or item[1] == 'NN']
     split_nouns = "|".join("%s" % tup[0] for tup in nouns)
     return split_nouns
-
-
-# def get_tags(text):
-#     pos = komoran.pos(text)
-#     # str_pos = ";".join(map(str, pos))
-#     str_pos = ";".join(map("/".join, pos))
-#     return str_pos
-
-# def get_sentiment(text):
-#     s = classifier.classify(text)
-#     return s
-
-# def get_nouns(text):
-#     # twitter = Twitter()
-#     nouns = okt.nouns(text)
-#     str_nouns = "|".join(nouns)
-#     return str_nouns
 
 df = read_data('./data/tweet_test.csv')
 
@@ -104,49 +61,14 @@ for row in df.itertuples():
 # 단어별로 자른 것 넣을 새로운 column 만들기
 df['word'] = ''
 
-# 명사별로 자르고 word column에 값 넣기
-# head로 설정해놔서 나중에 없애고 다시 테스트
-# komoran = Komoran()
-# for row in df.head().itertuples():
-#     word_str = get_tags(row.tweet)
-#     df.at[row.Index, 'word'] = word_str
-    # df.set_value(row.Index, 'word', word_str)
-
-# 트위터 클래스 변경
-# okt = Okt()
 h = Hannanum()
-for row in df.itertuples():
-    hangul_text = hangul.sub("",row.tweet)
+hangul = re.compile('[^ ㄱ-ㅣ가-힣]+')
+for row in df.head(20).itertuples():
+    hangul_text = hangul.sub("",row.tweet).replace("‘", "").replace("’", "").replace("“", "").replace("”", "")
     # URLless_string = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', hangul_text)
-    spell_ok = spell_checker.check(hangul_text)
+    spell_ok = spell_checker.check(str(hangul_text))
     word_str = get_nouns(str(spell_ok.checked))
     df.at[row.Index, 'word'] = word_str
 
-# 결측값 있을 시 제거
-# df['id'].dropna()
-
-# df['word'].head()
-
-
 # csv 저장
-df[['date', 'word', 'username', 'tweet']].to_csv("./data/result1.csv", mode="w")
-
-
-# test
-# print(df.dtypes)
- 
-# print(df['new'].head())
-# print(get_tags(df['tweet'].head()))
-
-# df['tweet1'] = pd.Series(get_tags(str(df['tweet'])))
-# print(get_tags(str(df['tweet'])))
-
-# okt = Okt()
-# list_d = okt.pos(str(df['tweet']))
-# df2 = pd.DataFrame()
-# print(list_d)
-
-# def tokenize(doc):
-#     return ['/'.join(t) for t in pos_tagger.pos(doc, norm=True, stem=True)]
-# train_docs1 = [(tokenize(row[1]), row[2]) for row in train_data]
-# print(train_docs1)
+# df[['date', 'word', 'username', 'tweet']].to_csv("./data/clean_data.csv", mode="w")
